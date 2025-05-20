fa51a1cb0c372a02b76f88a8b889ab638a488e1a
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2025-05-20 10:48:46 +0200

    fertig

--- src/MandelbrotModel.java
+++ src/MandelbrotModel.java
@@ -1,9 +1,13 @@
+import java.awt.Color;
+
 public class MandelbrotModel
 {
-    private int iter(double a, double b) {
     int MAX_ITERATIONS = 1000;
+    int PIXELHEIGHT;
+    int PIXELWIDTH;
 
-       
+    private int iter(double a, double b) {
+
         double x = 0.0;
         double y = 0.0;
         int iterations = 0;
@@ -18,6 +22,33 @@ public class MandelbrotModel
                 return MAX_ITERATIONS;
             } while (x <= 2 && y <= 2);
                 return iterations;
+    }
+
+    public void setPIXELHEIGHT(int height){
+        PIXELHEIGHT=height;
+    }
+
+    public void setPIXELWIDTH(int width){
+        PIXELWIDTH=width;
+    }
+
+    Color intToColor(int val){
+        //TODO write function
+        return Color.BLACK;
+    }
+
+    Color[][] generateColors(double xstart, double xend, double ystart, double yend){
+        Color[][] colorarray=new Color[PIXELWIDTH][PIXELHEIGHT];
+
+        for(int i=0;i<PIXELWIDTH;i++){
+            for(int o=0;o<PIXELHEIGHT;i++){
+                colorarray[i][o]=intToColor(iter(xstart+i*(xend-xstart)/PIXELWIDTH,ystart+i*(yend-ystart)/PIXELHEIGHT));
+            }
+
+
+        }
+
+        return colorarray;
     }
 
 }

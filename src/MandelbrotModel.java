93cbe5e6af15a9a879d4f62eafb33358d1bc0778
TilG7 <til.guhlmann2004@gmail.com>
2025-05-20 10:03:14 +0200

    erster

--- src/MandelbrotModel.java
+++ src/MandelbrotModel.java
@@ -1,3 +1,21 @@
 public class MandelbrotModel
 {
+    private boolean iter(double a, double b) {
+       
+        double x = 0.0;
+        double y = 0.0;
+        int iterations = 0;
+        
+        do {
+            double xnew = x * x - y * y + a;
+            double ynew = 2 * x * y + b;
+            x = xnew;
+            y = ynew;
+            iterations++;
+            if (iterations == MAX_ITERATIONS)
+                return MAX_ITERATIONS;
+            } while (x <= 2 && y <= 2);
+                return iterations;
+    }
+
 }

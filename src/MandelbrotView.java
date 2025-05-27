5fc62d814e5a634d4d0688905dfafcddc8e96e54
Frederic Egle <cynihx@gmail.com>
2025-05-27 09:38:12 +0200

    Fenster schließen beendet nun Anwendung

--- src/MandelbrotView.java
+++ src/MandelbrotView.java
@@ -40,6 +40,7 @@ public class MandelbrotView
         frame.add(panel, BorderLayout.SOUTH);
 
         frame.setSize(width, height+50); // text fields -> +50
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
